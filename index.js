4bf6220d254b19f5c62b76659981dcf97943c75b
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-11-07 15:57:51 +0400

    Setup session

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const hbs = require('express-hbs');
+const session = require('express-session');
 
 const app = express();
+
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
 
 // Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine(
@@ -15,8 +25,11 @@ app.set('views', __dirname + '/views');
 
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
+  req.session.views = req.session.views || 1;
+
   res.render('home', {
     btnName: 'login',
+    views: req.session.views++,
   });
 });
 app.listen(PORT, () => console.log(`server started at localhost:${PORT}`));
